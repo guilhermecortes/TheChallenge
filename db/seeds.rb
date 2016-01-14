0edaa36578c2d787ff2fe6c9847e7826ddd048ea
Guilherme <guilherme.cortes@handcom.com.br>
2016-01-14 21:26:22 -0200

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+venues = Venue.create([
+    {"name":"Papercut","latitude":59.317159,"longitude":18.054939},
+    {"name":"New Image Car Hi-Fi","latitude":51.58875,"longitude":-0.363208},
+    {"name":"Halfords","latitude":51.514127,"longitude":-0.100986},
+    {"name":"Carpet World","latitude":51.555871,"longitude":-0.072559},
+    {"name":"Harrow Breakers","latitude":51.5938,"longitude":-0.3529},
+    {"name":"Carpet World","latitude":51.549684,"longitude":-0.239651},
+    {"name":"Caf Pascal","latitude":59.341935,"longitude":18.052073},
+    {"name":"SACHAJUAN","latitude":59.340186,"longitude":18.076286},
+    {"name":"Heather Bird Health of Knightsbridge Ltd","latitude":51.497593,"longitude":-0.164862},
+    {"name":"Marks \u0026 Spencer","latitude":51.530506,"longitude":-0.12563},
+    {"name":"Ac bergs Mat \u0026 Catering","latitude":59.337147,"longitude":18.028672},
+    {"name":"Vita","latitude":59.334045,"longitude":18.076777},
+    {"name":"Herr Judit","latitude":59.31758,"longitude":18.053943},
+    {"name":"Nitty Gritty","latitude":59.317155,"longitude":18.054099},
+    {"name":"Jupiter AB","latitude":59.318283,"longitude":18.059026},
+    {"name":"Haberdash","latitude":59.342045,"longitude":18.048814},
+    {"name":"P J Car Audio","latitude":51.593823,"longitude":-0.334988},
+    {"name":"Car Audio and Security","latitude":51.517904,"longitude":-0.13134},
+    {"name":"Citysounds","latitude":51.518881,"longitude":-0.132556},
+    {"name":"Salong Fogde","latitude":59.276748,"longitude":18.126838}
+])
